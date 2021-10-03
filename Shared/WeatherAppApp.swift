5efa302e1e077c7c88d31ef467fea085a8b2414e
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2021-10-03 17:08:18 +0900

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Guillermo Ignacio Enriquez Gutierrez on 2021/10/03.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
